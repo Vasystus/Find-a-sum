c19065fa13fceee763809d98d62b1e271f82213a
Vasystus <artgalla8@gmail.com>
2021-11-07 19:58:03 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# web-platform-zjhkyf
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/web-platform-zjhkyf)
---
+++ script.js
@@ -0,0 +1,9 @@
+function addTwoNum() {
+               
+ let numberOne = parseInt(document.getElementById("num1").value);
+  let numberTwo = parseInt(document.getElementById("num2").value);
+  let result = numberOne +numberTwo;
+
+  document.getElementById("output").value = result;
+}
+
